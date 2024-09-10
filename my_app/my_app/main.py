f46882c048cb07e71cc50ad40d0f74ab43bbb02f
tgkim911021 <tgkim911021@gmail.com>
2024-09-10 10:13:46 +0900

    #1 add : myapps

---
+++ my_app/my_app/main.py
@@ -0,0 +1,7 @@
+def add(a: int, b: int) -> int:
+    """Adds two numbers and returns the result."""
+    return a + b
+
+if __name__ == "__main__":
+    result = add(2, 3)
+    print(f"2 + 3 = {result}")
